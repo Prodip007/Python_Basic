30fea8cdc2eef9addbba01e1e56b38fcd50244c7
Prodip007 <p.k.sorkar007@gmail.com>
2022-08-12 23:45:25 +0600

    List.py and Greetings.txt files added

---
+++ Lists.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    N = int(input())
+
+main_list = []
+for x in range(N):
+    raw_data = [input().split()]
+    print(raw_data)
+for y in range(len(raw_data)):
+    print(y)
+        # raw_data[y] = int(raw_data[y])
+
+    # if x[0] == 'insert':
+    #     main_list.insert(x[1], x[2])
+
+# print(main_list)
